9848a80f4673b78d933323f0a89d2f36b4870715
Aditya Dubey <dubeyaditya6232@gmail.com>
2025-04-20 00:47:00 +0530

    added did you know section and a better html template

--- articles.js
+++ articles.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+const QUERY = `
+"generative AI" OR "GenAI" OR "LLM" OR "AI agent" OR "AI API" OR "AI-powered" OR "AI application" OR "AI workflow" OR "AI tool"
+`;
+
 function getLast7DaysRange() {
     const today = new Date();
     const sevenDaysAgo = new Date(today);
@@ -14,7 +18,7 @@ function getLast7DaysRange() {
 export const fetchFromNewsAPI = async () => {
     const { fromDate, toDate } = getLast7DaysRange();
 
-    const url = `https://newsapi.org/v2/everything?q=generative+AI+OR+LLM+OR+OpenAI&from=${fromDate}&to=${toDate}&sortBy=publishedAt&language=en&pageSize=25&apiKey=${process.env.NEWS_API_KEY}`;
+    const url = `https://newsapi.org/v2/everything?q=${encodeURIComponent(QUERY)}&from=${fromDate}&to=${toDate}&sortBy=publishedAt&language=en&pageSize=25&apiKey=${process.env.NEWS_API_KEY}`;
     const response = await axios.get(url);
     if (response.status !== 200) {
         console.error('Error fetching from NewsAPI:', response.statusText);
@@ -34,7 +38,7 @@ export const fetchFromNewsAPI = async () => {
 
 export const fetchFromGNews = async () => {
     const { fromDate, toDate } = getLast7DaysRange();
-    const url = `https://gnews.io/api/v4/search?q=generative%20AI&lang=en&from=${fromDate}&to=${toDate}&max=25&token=${process.env.GNEWS_API_KEY}`;
+    const url = `https://gnews.io/api/v4/search?q=${encodeURIComponent(QUERY)}I&lang=en&from=${fromDate}&to=${toDate}&max=25&token=${process.env.GNEWS_API_KEY}`;
     const response = await axios.get(url);
     if (response.status !== 200) {
         console.error('Error fetching from GNewsAPI:', response.statusText);
